9945190304cc72d34527c6ae6225b0b05a21201b
l181054meerabchishti <46877062+l181054meerabchishti@users.noreply.github.com>
2019-01-21 10:56:37 +0500

    Add files via upload
    This one is incomplete.

---
+++ q1_l181054.cpp
@@ -0,0 +1,26 @@
+// q1_l181054.cpp : Defines the entry point for the console application.
+//
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	 
+    cout << "Please enter a number";
+ 
+	 int a;
+  cin>>a;
+  for(int i=1;i<=a;i++)
+  {
+    for(int j=0;j<i;j++)
+	
+      cout<<i;
+      cout<<"  ";
+	  cout<<endl;
+	
+  }
+	return 0;
+
+}
